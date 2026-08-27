c04159aeb9506000a2960634deacacaae7df2063
Yash Singh <yashsinghss047@gmail.com>
2026-08-27 22:56:12 +0530

    added push function in replay_buffer

--- agent/replay_buffer.py
+++ agent/replay_buffer.py
@@ -9,6 +9,9 @@ class ReplayBuffer:
     def __init__(self, capacity = 20000):
         self.buffer = deque(maxlen = capacity)
 
+    def push(self, state, action, reward, next_state, done):
+        self.buffer.append((state, action, reward, next_state, done))
+
     def sample(self, batch_size):
         batch = random.sample(self.buffer, batch_size)
         states, actions, rewards, next_states, dones = zip(*batch)
